caad0d627d531989be097f08713e23a1d0892edb
LostCore <lostcore@gmail.com>
2013-08-08 09:36:39 +0200

    Fix vari

--- classes/controller_template.php
+++ classes/controller_template.php
@@ -9,18 +9,16 @@ class Controller_Template extends \Fuel\Core\Controller_Template{
     public function __construct(\Request $request){
         parent::__construct($request);
         $this->language_info['default'] = \Intl::forge()->getDefaultLanguage();
-        $this->language_info['active'] = \Config::get('multilang.active_languages');
         $this->language_info['additionals'] = \Config::get('multilang.additional_languages');
     }
 
 	public function before(){
         parent::before();
-        \Config::load("multilang",true);
-        \Config::set("multilang.rewrite_uri",false);
+        \Config::load("i18n",true);
+        \Config::set("i18n.rewrite_uri",false);
 
         if(\Config::get('multilang.active')){
             $this->template->set_global('default_language',$this->language_info['default']);
-            $this->template->set_global('active_languages',$this->language_info['active']);
             $this->template->set_global('additional_languages',$this->language_info['additionals']);
         }
     }
