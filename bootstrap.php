206b8adb7e77727dd8c9c47cf2b802c2f6628484
LostCore <lostcore@gmail.com>
2013-08-09 11:07:06 +0200

    Fix vari

--- bootstrap.php
+++ bootstrap.php
@@ -12,7 +12,6 @@ Autoloader::add_core_namespace('Multilang');
 Autoloader::add_classes(array(
 	'Multilang\\Model'                  		=> __DIR__.'/classes/model.php',
     'Multilang\\Controller_Template'    		=> __DIR__.'/classes/controller_template.php',
-    'Multilang\\Uri'                    		=> __DIR__.'/classes/uri.php',
     'Multilang\\Observer_Multilang'     		=> __DIR__.'/classes/observers/multilang.php',
     'Multilang\\Observer_Slug'          		=> __DIR__.'/classes/observers/slug.php'
 ));
